01f30f4af7deb33b1b4879db28dac990d9f54c52
Shahjan00 <shahjanali0022@gmail.com>
2026-04-03 14:43:57 -0400

    added postgres config

--- Order_Management/settings.py
+++ Order_Management/settings.py
@@ -82,10 +82,7 @@ WSGI_APPLICATION = 'Order_Management.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
